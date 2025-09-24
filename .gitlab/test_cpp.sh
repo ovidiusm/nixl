e580adb4dac45149ecd41eaf5b12656d6311cbe1
Ovidiu Mara <ovidium@nvidia.com>
2025-09-24 14:58:00 +0200

    Run gtest directly
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>

--- .gitlab/test_cpp.sh
+++ .gitlab/test_cpp.sh
@@ -84,7 +84,7 @@ kill -s INT $telePID
 ./bin/serdes_test
 
 # shellcheck disable=SC2154
-gtest-parallel --workers=1 --serialize_test_cases ./bin/gtest -- --min-tcp-port="$min_gtest_port" --max-tcp-port="$max_gtest_port"
+./bin/gtest -- --min-tcp-port="$min_gtest_port" --max-tcp-port="$max_gtest_port"
 ./bin/test_plugin
 
 # Run NIXL client-server test
